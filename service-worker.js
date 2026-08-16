ab67e7296795b96376f4fabd7b5e5e9016a746d0
marcosmuralo <mmuralo@gmail.com>
2026-08-15 21:15:51 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "murairon-v3-4-project-95";
+const CACHE = "murairon-v3-5-project-95";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
